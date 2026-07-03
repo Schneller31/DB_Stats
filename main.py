c1027e9f1fa2d49a7b31f171011dcffb26a623fa
Schneller31 <philipp.schneller3109@gmail.com>
2026-07-03 22:44:03 +0200

    added "sorting" system for output

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 import requests
+import xml.etree.ElementTree as ET
 
+# variables
 with open("api.json", "r") as file:
     api_keys = json.load(file)
-
 
 url = "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/fchg/8000157"
 
@@ -13,11 +14,25 @@ headers = {
     "accept": "application/xml"
 }
 
-
 response = requests.get(url, headers=headers)
+
+root = ET.fromstring(response.text)
+
+
+# sorting
+for s in root.findall("s"):
+    for ar in s.findall("ar"):
+        zug = ar.get("l")
+        if zug:
+            print("Zug:", zug)
+
+    for dp in s.findall("dp"):
+        zug = dp.get("l")
+        if zug:
+            print("Zug:", zug)
 
 print(response.status_code)
 print(response.text)
 
-print(api_keys["DB_CLIENT_ID"])
-print(api_keys["DB_API_KEY"])
+#print(api_keys["DB_CLIENT_ID"])
+#print(api_keys["DB_API_KEY"])
