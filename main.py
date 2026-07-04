aa705a36fc2741dfe190e765808c0cef8f2e9fe0
Schneller31 <philipp.schneller3109@gmail.com>
2026-07-04 12:51:27 +0200

    various changes to display output correctly

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import requests
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
-# variables
+# VARIABLES
 with open("api.json", "r") as file:
     api_keys = json.load(file)
 
@@ -13,27 +14,60 @@ headers = {
     "DB-Api-Key": api_keys["DB_API_KEY"],
     "accept": "application/xml"
 }
+
 response = requests.get(url, headers=headers)
 
 root = ET.fromstring(response.text)
 
+bahnhof = "Heilbronn Hbf"
 
-# sorting
+# LIST
 zuege = []
 
 for s in root.findall("s"):
-    for ar in s.findall("ar"):   # arrivals
+
+    for ar in s.findall("ar"): # 'ar' bedeutet Ankunft
         zug = ar.get("l")
-        if zug:
-            zuege.append(zug)
 
-    for dp in s.findall("dp"):   # departures
+        if zug:
+            zuege.append({
+                "bahnhof": bahnhof,
+                "zug": zug,
+                "typ": "Ankunft",
+                "geplant": ar.get("pt"),
+                "aktuell": ar.get("ct")
+            })
+
+    for dp in s.findall("dp"): # 'dp' bedeutet Abfahrt
         zug = dp.get("l")
+
         if zug:
-            zuege.append(zug)
+            zuege.append({
+                "bahnhof": bahnhof,
+                "zug": zug,
+                "typ": "Abfahrt",
+                "geplant": dp.get("pt"),
+                "aktuell": dp.get("ct")
+            })
 
-# output
-print("Gefundene Züge:")
-print(zuege)
+# OUTPUT
+print("Gefundene Züge:\n")
 
-#print(response.text)
+for zug in zuege:
+
+    zeit = zug["geplant"] if zug["geplant"] else zug["aktuell"]
+
+    # Zeit lesbar machen
+    if zeit:
+        zeit = datetime.strptime(zeit, "%y%m%d%H%M").strftime("%d.%m.%Y %H:%M")
+    else:
+        zeit = "Keine Zeit"
+
+    print(
+        f"{zug['typ']:9} | "
+        f"{zug['zug']:8} | "
+        f"{zeit} | "
+        f"{zug['bahnhof']}"
+    )
+
+# print(response.text)
